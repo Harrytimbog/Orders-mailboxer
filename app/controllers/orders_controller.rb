f2b3f0ab4cdc70f07a6be09834d612ab7091f891
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-05-27 14:39:40 +0100

    create home page and add flashes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,25 @@
 class OrdersController < ApplicationController
-    def new
-      @order = Order.new
-    end
-  
-    def create
-      @order = Order.new(order_params)
-  
-      if @order.save
-        OrderMailer.with(order: @order).new_order_email.deliver_later
+  def new
+    @order = Order.new
+  end
 
-        flash[:success] = t('flash.order.success')
-        redirect_to root_path
-      else
-        flash.now[:error] = t('flash.order.error_html')
-        render :new
-      end
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      OrderMailer.with(order: @order).new_order_email.deliver_later
+
+      flash[:success] = t('flash.order.success')
+      redirect_to root_path, notice: 'Order was successfully sent'
+    else
+      flash.now[:error] = t('flash.order.error_html')
+      render :new, notice: 'something went wrong'
     end
-  
-    private
-  
-    def order_params
-      params.require(:order).permit(:name, :email, :address, :phone, :message)
-    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :email, :address, :phone, :message)
+  end
 end
-  
